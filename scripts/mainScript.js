9d5e80f97170778cdfb1a59c8fff70f5a96df4a7
justinsteinberg <justin.steinberg1@aol.com>
2023-07-12 16:17:54 -0400

    Making letters light up on hover.

---
+++ scripts/mainScript.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$(".randColor").hover(function(){
+		var color = 'rgb('+ Math.floor(Math.random()*256).toString() + ',' + Math.floor(Math.random()*256).toString() + ',' + Math.floor(Math.random()*256).toString()  + ')';
+		console.log(color);
+		$(this).css("background-color", color);
+	}, function(){
+		$(this).css("background-color", "floralwhite");
+	});
+});

fcb6e037f3928f4b866466b0d2e6d57fea7c09ef
justinsteinberg <justin.steinberg1@aol.com>
2023-07-20 16:40:57 -0400

    Adding Marquee.

--- scripts/flexTest.js
+++ scripts/flexTest.js
@@ -5,71 +5,71 @@ $(document).ready(function(){
 	$(".hover2").hover(function(){
 		
 		var flexHover = "0.55 1 55%";
-		$(this).css("flex", flexHover);
-		$(this).css("height", "77vh");
-		$(".work").css("flex", "0.45 1 45%");
-		$(".work").css("height", "57vh");
-		$(".about").css("flex", "0.25 1 25%");
-		$(".about").css("height", "43vh");
-		$(".about").css("top", "-20vh");
-		$(".logo").css("flex", "0 1 20%")
-		$(".logo").css("margin-right","30vw");
-		$(".logo").css("height", "20vh");
-		$(".logo").css("top", "-20vh");		
-		$(".community").css("height", "23vh");
-		$(".community").css("top", "-20vh");
-		$(".community").css("flex", "0.75 1 75%");
+		$(this).stop(true,true).css("flex", flexHover);
+		$(this).stop(true,true).css("height", "77vh");
+		$(".work").stop(true,true).css("flex", "0.45 1 45%");
+		$(".work").stop(true,true).css("height", "57vh");
+		$(".about").stop(true,true).css("flex", "0.25 1 25%");
+		$(".about").stop(true,true).css("height", "43vh");
+		$(".about").stop(true,true).css("top", "-20vh");
+		$(".logo").stop(true,true).css("flex", "0 1 20%")
+		$(".logo").stop(true,true).css("margin-right","30vw");
+		$(".logo").stop(true,true).css("height", "20vh");
+		$(".logo").stop(true,true).css("top", "-20vh");		
+		$(".community").stop(true,true).css("height", "23vh");
+		$(".community").stop(true,true).css("top", "-20vh");
+		$(".community").stop(true,true).css("flex", "0.75 1 75%");
 				
 
 	}, function(){
 
 		var flexUnhover = "0.4 1 40%";
-		$(this).css("flex", flexUnhover);
-		$(this).css("height", "60vh");
-		$(".work").css("flex", "0.60 1 60%");
-		$(".work").css("height", "40vh");
-		$(".about").css("flex", "0.40 1 40%");
-		$(".about").css("height", "60vh");
-		$(".community").css("height", "40vh");
-		$(".community").css("top", "-20vh");
-		$(".community").css("flex", "0.60 1 60%");
-		$(".logo").css("margin-right","0vw");
+		$(this).stop(true,true).css("flex", flexUnhover);
+		$(this).stop(true,true).css("height", "60vh");
+		$(".work").stop(true,true).css("flex", "0.60 1 60%");
+		$(".work").stop(true,true).css("height", "40vh");
+		$(".about").stop(true,true).css("flex", "0.40 1 40%");
+		$(".about").stop(true,true).css("height", "60vh");
+		$(".community").stop(true,true).css("height", "40vh");
+		$(".community").stop(true,true).css("top", "-20vh");
+		$(".community").stop(true,true).css("flex", "0.60 1 60%");
+		$(".logo").stop(true,true).css("margin-right","0vw");
 
 	});
 	//* Work
 
 	$(".hover1").hover(function(){
 		
+
 		var flexHover = "0.75 1 75%";
-		$(this).css("flex", flexHover);
-		$(this).css("height", "55vh");
-		$(".connect").css("flex", "0.25 1 25%");
-		$(".connect").css("height", "75vh");
-		$(".about").css("flex", "0.55 1 55%");
-		$(".about").css("height", "45vh");
-		$(".about").css("top", "-20vh");
-		$(".logo").css("flex", "0 1 20%")
-		$(".logo").css("margin-left","30vw");
-		$(".logo").css("height", "20vh");
-		$(".logo").css("top", "-20vh");
-		$(".community").css("height", "25vh");
-		$(".community").css("top", "-20vh");
-		$(".community").css("flex", ".45 1 45%");
+		$(this).stop(true,true).css("flex", flexHover);
+		$(this).stop(true,true).css("height", "55vh");
+		$(".connect").stop(true,true).css("flex", "0.25 1 25%");
+		$(".connect").stop(true,true).css("height", "75vh");
+		$(".about").stop(true,true).css("flex", "0.55 1 55%");
+		$(".about").stop(true,true).css("height", "45vh");
+		$(".about").stop(true,true).css("top", "-20vh");
+		$(".logo").stop(true,true).css("flex", "0 1 20%")
+		$(".logo").stop(true,true).css("margin-left","30vw");
+		$(".logo").stop(true,true).css("height", "20vh");
+		$(".logo").stop(true,true).css("top", "-20vh");
+		$(".community").stop(true,true).css("height", "25vh");
+		$(".community").stop(true,true).css("top", "-20vh");
+		$(".community").stop(true,true).css("flex", ".45 1 45%");
 
 	}, function(){
 		
-
 		var flexUnhover = "0.6 1 60%";
-		$(this).css("flex", flexUnhover);
-		$(this).css("height", "40vh");
-		$(".connect").css("flex", "0.40 1 40%");
-		$(".connect").css("height", "60vh");
-		$(".about").css("flex", "0.40 1 0");
-		$(".about").css("height", "60vh");
-		$(".community").css("height", "40vh");
-		$(".community").css("top", "-20vh");
-		$(".community").css("flex", "0.60 1 0");
-		$(".logo").css("margin-left","0vw");
+		$(this).stop(true,true).css("flex", flexUnhover);
+		$(this).stop(true,true).css("height", "40vh");
+		$(".connect").stop(true,true).css("flex", "0.40 1 40%");
+		$(".connect").stop(true,true).css("height", "60vh");
+		$(".about").stop(true,true).css("flex", "0.40 1 0");
+		$(".about").stop(true,true).css("height", "60vh");
+		$(".community").stop(true,true).css("height", "40vh");
+		$(".community").stop(true,true).css("top", "-20vh");
+		$(".community").stop(true,true).css("flex", "0.60 1 0");
+		$(".logo").stop(true,true).css("margin-left","0vw");
 
 	});
 
@@ -78,38 +78,38 @@ $(document).ready(function(){
 	$(".hover5").hover(function(){
 
 		var flexHover = ".65 1 65%";
-		$(this).css("flex", flexHover);
-		$(this).css("height", "46vh");
-		$(this).css("top", "-20vh");
-		$(".connect").css("flex", ".45 1 45%");
-		$(".connect").css("height", "54vh");
-		$(".connect").css("position", "static");
-		$(".about").css("flex", ".35 1 35%");
-		$(".about").css("height", "66vh");
-		$(".about").css("top", "-20vh");
-		$(".logo").css("flex", "0 1 20%");
-		$(".logo").css("top", "-20vh");
-		$(".work").css("height", "34vh");
-		$(".work").css("flex", "0.55 1 55%");
-		$(".logo").css("margin-right","10vw");
-		$(".logo").css("height", "20vh");
-		$(".about").css("top", "-20vh");
-		$(".community").css("top", "-20vh");
+		$(this).stop(true,true).css("flex", flexHover);
+		$(this).stop(true,true).css("height", "46vh");
+		$(this).stop(true,true).css("top", "-20vh");
+		$(".connect").stop(true,true).css("flex", ".45 1 45%");
+		$(".connect").stop(true,true).css("height", "54vh");
+		$(".connect").stop(true,true).css("position", "static");
+		$(".about").stop(true,true).css("flex", ".35 1 35%");
+		$(".about").stop(true,true).css("height", "66vh");
+		$(".about").stop(true,true).css("top", "-20vh");
+		$(".logo").stop(true,true).css("flex", "0 1 20%");
+		$(".logo").stop(true,true).css("top", "-20vh");
+		$(".work").stop(true,true).css("height", "34vh");
+		$(".work").stop(true,true).css("flex", "0.55 1 55%");
+		$(".logo").stop(true,true).css("margin-right","10vw");
+		$(".logo").stop(true,true).css("height", "20vh");
+		$(".about").stop(true,true).css("top", "-20vh");
+		$(".community").stop(true,true).css("top", "-20vh");
 
 	}, function(){
 
 		var flexUnhover = "0.60 1 60%";
-		$(this).css("flex", flexUnhover);
-		$(this).css("height", "40vh");
-		$(".connect").css("flex", "0.40 1 40%");
-		$(".connect").css("height", "60vh");
-		$(".about").css("flex", "0.40 1 40%");
-		$(".about").css("height", "60vh");
-		$(".community").css("top", "-20vh");
-		$(".logo").css("top", "-20vh")
-		$(".work").css("flex", "0.60 1 60%");
-		$(".work").css("height", "40vh");
-		$(".logo").css("margin-right","0vw");
+		$(this).stop(true,true).css("flex", flexUnhover);
+		$(this).stop(true,true).css("height", "40vh");
+		$(".connect").stop(true,true).css("flex", "0.40 1 40%");
+		$(".connect").stop(true,true).css("height", "60vh");
+		$(".about").stop(true,true).css("flex", "0.40 1 40%");
+		$(".about").stop(true,true).css("height", "60vh");
+		$(".community").stop(true,true).css("top", "-20vh");
+		$(".logo").stop(true,true).css("top", "-20vh")
+		$(".work").stop(true,true).css("flex", "0.60 1 60%");
+		$(".work").stop(true,true).css("height", "40vh");
+		$(".logo").stop(true,true).css("margin-right","0vw");
 
 	});
 
@@ -118,37 +118,37 @@ $(document).ready(function(){
 	$(".hover4").hover(function(){
 
 		var flexHover = "0.55 1 55%";
-		$(this).css("flex", flexHover);
-		$(this).css("height", "77vh");
-		$(".work").css("flex", "0.75 1 75%");
-		$(".work").css("height", "23vh");
-		$(".connect").css("flex", "0.25 1 25%");
-		$(".connect").css("height", "43vh");
-		$(".connect").css("position", "static");
-		$(".logo").css("flex", "0 1 20%");
-		$(".logo").css("margin-left","30vw");
-		$(".logo").css("height", "20vh");
-		$(".logo").css("top", "-20vh");
-		$(".community").css("height", "57vh");
-		$(".community").css("flex", "0.45 1 45%");
-		$(".community").css("top", "-20vh");
-		$(".about").css("top", "-20vh");
-		$(".community").css("top", "-20vh");
+		$(this).stop(true,true).css("flex", flexHover);
+		$(this).stop(true,true).css("height", "77vh");
+		$(".work").stop(true,true).css("flex", "0.75 1 75%");
+		$(".work").stop(true,true).css("height", "23vh");
+		$(".connect").stop(true,true).css("flex", "0.25 1 25%");
+		$(".connect").stop(true,true).css("height", "43vh");
+		$(".connect").stop(true,true).css("position", "static");
+		$(".logo").stop(true,true).css("flex", "0 1 20%");
+		$(".logo").stop(true,true).css("margin-left","30vw");
+		$(".logo").stop(true,true).css("height", "20vh");
+		$(".logo").stop(true,true).css("top", "-20vh");
+		$(".community").stop(true,true).css("height", "57vh");
+		$(".community").stop(true,true).css("flex", "0.45 1 45%");
+		$(".community").stop(true,true).css("top", "-20vh");
+		$(".about").stop(true,true).css("top", "-20vh");
+		$(".community").stop(true,true).css("top", "-20vh");
 
 	}, function(){
 
 		var flexUnhover = "0.4 1 0%";
-		$(this).css("flex", flexUnhover);
-		$(this).css("height", "60vh");
-		$(".work").css("flex", "0.60 1 60%");
-		$(".work").css("height", "40vh");
-		$(".connect").css("flex", "0.40 1 40%");
-		$(".connect").css("height", "60vh");
-		$(".community").css("height", "40vh");
-		$(".community").css("top", "-20vh");
-		$(".community").css("flex", "0.60 1 0");
-		$(".community").css("top", "-20vh");
-		$(".logo").css("margin-left","0vw");
+		$(this).stop(true,true).css("flex", flexUnhover);
+		$(this).stop(true,true).css("height", "60vh");
+		$(".work").stop(true,true).css("flex", "0.60 1 60%");
+		$(".work").stop(true,true).css("height", "40vh");
+		$(".connect").stop(true,true).css("flex", "0.40 1 40%");
+		$(".connect").stop(true,true).css("height", "60vh");
+		$(".community").stop(true,true).css("height", "40vh");
+		$(".community").stop(true,true).css("top", "-20vh");
+		$(".community").stop(true,true).css("flex", "0.60 1 0");
+		$(".community").stop(true,true).css("top", "-20vh");
+		$(".logo").stop(true,true).css("margin-left","0vw");
 
 	});
 
@@ -156,27 +156,27 @@ $(document).ready(function(){
 	$(".hover3").hover(function(){
 		/*$(".logo").css("visibility", "hidden");*/
 		var flexHover = "0 1 40%";
-		$(".logo").css("flex", flexHover);
-		$(".logo").css("z-index", "99");
-		$(".logo").css("order", "1");
-		$(".logo").css("height", "40vh");
-		$(this).css("top","-40vh");
-		$(".work").css("flex", "0.70 1 70%");
-		$(".work").css("height", "30vh");
-		$(".connect").css("flex", "0.30 1 30%");
-		$(".connect").css("height", "70vh");
-		$(".connect").css("position", "relative");
-		$(".about").css("flex", "0.3 1 30%");
-		$(".about").css("height", "70vh");
-		$(".about").css("top", "-60vh");
+		$(".logo").stop(true,true).css("flex", flexHover);
+		$(".logo").stop(true,true).css("z-index", "99");
+		$(".logo").stop(true,true).css("order", "1");
+		$(".logo").stop(true,true).css("height", "40vh");
+		$(this).stop(true,true).css("top","-40vh");
+		$(".work").stop(true,true).css("flex", "0.70 1 70%");
+		$(".work").stop(true,true).css("height", "30vh");
+		$(".connect").stop(true,true).css("flex", "0.30 1 30%");
+		$(".connect").stop(true,true).css("height", "70vh");
+		$(".connect").stop(true,true).css("position", "relative");
+		$(".about").stop(true,true).css("flex", "0.3 1 30%");
+		$(".about").stop(true,true).css("height", "70vh");
+		$(".about").stop(true,true).css("top", "-60vh");
 		//*$(".about").css("top","0vh");
-		$(".community").css("flex", "0.7 0 70%");
-		$(".community").css("height", "30vh");
-		$(".community").css("top", "-60vh");
+		$(".community").stop(true,true).css("flex", "0.7 0 70%");
+		$(".community").stop(true,true).css("height", "30vh");
+		$(".community").stop(true,true).css("top", "-60vh");
 
 	
 	}, function(){
-	
+
 	/*	var flexUnhover = "0.2 1 0%";	
 		$(".hidden-box").css("display", "none");
 		$(this).css("flex", flexUnhover)

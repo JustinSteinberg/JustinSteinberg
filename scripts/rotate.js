e9f4bd419835069b51ed22b2afd2c29b2aebb11c
justinsteinberg <justin.steinberg1@aol.com>
2023-08-04 17:57:18 -0400

    Adding rotating cube onb skills page.

---
+++ scripts/rotate.js
@@ -0,0 +1,19 @@
+const cube = document.querySelector(".cube");
+let mouseX = 0;
+let mouseY = 0;
+const rotationValue = 270;
+
+const handleMouseMove = (event) => {
+	mouseX = event.clientX;
+	mouseY = event.clientY;
+	console.log("MouseX: ", mouseX);
+	console.log("MouseY: ", mouseY);
+	rotateX = -(mouseY / window.innerHeight - 0.5) * rotationValue;
+	rotateY = (mouseX / window.innerWidth - 0.5) * rotationValue;
+	console.log("rotateX: ", rotateX);
+	console.log("rotate: ", rotateY);
+	cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+};
+
+window.addEventListener("mousemove", handleMouseMove);
+window.addEventListener("touchmove", handleMouseMove);

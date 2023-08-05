5aa4dd90f0752767f4fe4a2d36e94d1ccf7b9a9f
justinsteinberg <justin.steinberg1@aol.com>
2023-08-05 13:19:35 -0400

    Adding rotating cube on skills page working with touchmove.

--- scripts/rotate.js
+++ scripts/rotate.js
@@ -15,5 +15,18 @@ const handleMouseMove = (event) => {
 	cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 };
 
+const handleTouchMove = (event) => {
+	mouseX = event.touches[0].clientX;
+	mouseY = event.touches[0].clientY;
+	console.log("TouchX: ", mouseX);
+	console.log("TouchY: ", mouseY);
+	rotateX = -(mouseY / window.innerHeight - 0.5) * rotationValue;
+	rotateY = (mouseX / window.innerWidth - 0.5) * rotationValue;
+	console.log("rotateX: ", rotateX);
+	console.log("rotate: ", rotateY);
+	cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+};
+
+
 window.addEventListener("mousemove", handleMouseMove);
-window.addEventListener("touchmove", handleMouseMove);
+window.addEventListener("touchmove", handleTouchMove);

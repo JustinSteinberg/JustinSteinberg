f7797509894467fb747966e817aad6bcc72a028b
justinsteinberg <justin.steinberg1@aol.com>
2023-08-05 23:15:10 -0400

    Getting rid of seemingly unnecessary code to hopefully improve performance.

--- scripts/rotate.js
+++ scripts/rotate.js
@@ -37,25 +37,13 @@ const handleTouchMove = (event) => {
 };
 
 cube.addEventListener('touchstart', function(e) {
-	prevRotateX = parseInt(cube.dataset.dataX);
-	prevRotateY = parseInt(cube.dataset.dataY);
-	console.log("End Rotation Pos X: ", prevRotateX);
 
    document.documentElement.style.overflow = 'hidden';
   
-	cube.style.transform = `rotateX(${prevRotateX}deg) rotateY(${prevRotateY}deg)`;
 });
 
 cube.addEventListener('touchend', function(e) {
    document.documentElement.style.overflow = 'auto';
-   /* let EndmouseX = e.changedTouches[0].pageX;
-	let EndmouseY = e.changedTouches[0].pageY;
-	console.log("ChangedTouchX: ", EndmouseX);
-	console.log("ChangedTouchY: ", EndmouseY);
-	prevRotateX = -(EndmouseY / window.innerHeight - 0.5) * rotationValue;
-	prevRotateY = (EndmouseX/ window.innerWidth - 0.5)  * rotationValue;
-	cube.dataset.dataX = prevRotateX;
-	cube.dataset.dataY = prevRotateY;*/
 	cube.dataset.dataX = parseInt(cube.dataset.rotateX);
 	cube.dataset.dataY = parseInt(cube.dataset.rotateY);
 });

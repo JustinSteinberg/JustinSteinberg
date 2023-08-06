ae47d202fb33e7242a98048b8f048018bd767c6b
justinsteinberg <justin.steinberg1@aol.com>
2023-08-05 23:06:09 -0400

    Using data attribute and a combination of touchmove touchstart and touchend to get continuous rotations.

--- scripts/rotate.js
+++ scripts/rotate.js
@@ -1,6 +1,11 @@
 const cube = document.querySelector(".cube");
-let mouseX;
-let mouseY;
+let mouseX = 0;
+let mouseY = 0;
+let prevRotateX = 0;
+let prevRotateY = 0;
+let rotateX = 0;
+let rotateXY = 0;
+
 const rotationValue = 270;
 
 const handleMouseMove = (event) => {
@@ -16,24 +21,43 @@ const handleMouseMove = (event) => {
 };
 
 const handleTouchMove = (event) => {
+	
 	mouseX = event.touches[0].pageX;
 	mouseY = event.touches[0].pageY;
 	console.log("TouchX: ", mouseX);
 	console.log("TouchY: ", mouseY);
-	rotateX = -(mouseY / window.innerHeight - 0.5) * rotationValue;
-	rotateY = (mouseX / window.innerWidth - 0.5) * rotationValue;
-	console.log("rotateX: ", rotateX);
-	console.log("rotate: ", rotateY);
-	cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 
+	rotateX = (-(mouseY / window.innerHeight - 0.5) * rotationValue) + parseInt(cube.dataset.dataX);
+	rotateY = ((mouseX/ window.innerWidth - 0.5)  * rotationValue) + parseInt(cube.dataset.dataY);
+	console.log("rotateX: ", rotateX);
+	console.log("rotateY: ", rotateY);
+	cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+	cube.dataset.rotateX = rotateX;
+	cube.dataset.rotateY = rotateY;
 };
 
 cube.addEventListener('touchstart', function(e) {
-    document.documentElement.style.overflow = 'hidden';
+	prevRotateX = parseInt(cube.dataset.dataX);
+	prevRotateY = parseInt(cube.dataset.dataY);
+	console.log("End Rotation Pos X: ", prevRotateX);
+
+   document.documentElement.style.overflow = 'hidden';
+  
+	cube.style.transform = `rotateX(${prevRotateX}deg) rotateY(${prevRotateY}deg)`;
 });
 
 cube.addEventListener('touchend', function(e) {
-    document.documentElement.style.overflow = 'auto';
+   document.documentElement.style.overflow = 'auto';
+   /* let EndmouseX = e.changedTouches[0].pageX;
+	let EndmouseY = e.changedTouches[0].pageY;
+	console.log("ChangedTouchX: ", EndmouseX);
+	console.log("ChangedTouchY: ", EndmouseY);
+	prevRotateX = -(EndmouseY / window.innerHeight - 0.5) * rotationValue;
+	prevRotateY = (EndmouseX/ window.innerWidth - 0.5)  * rotationValue;
+	cube.dataset.dataX = prevRotateX;
+	cube.dataset.dataY = prevRotateY;*/
+	cube.dataset.dataX = parseInt(cube.dataset.rotateX);
+	cube.dataset.dataY = parseInt(cube.dataset.rotateY);
 });
 
 window.addEventListener("mousemove", handleMouseMove);
